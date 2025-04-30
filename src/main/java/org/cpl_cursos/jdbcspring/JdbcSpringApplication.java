406e0429e0a0cd0e2cd5a2bd7cc653355c70aae0
CPL <carlos@centropyme.com>
2025-04-30 12:53:04 +0200

    Commit inicial con pom.xml desde Spring.initializr

---
+++ src/main/java/org/cpl_cursos/jdbcspring/JdbcSpringApplication.java
@@ -0,0 +1,13 @@
+package org.cpl_cursos.jdbcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cpl_cursos/jdbcspring/JdbcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cpl_cursos.jdbcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
