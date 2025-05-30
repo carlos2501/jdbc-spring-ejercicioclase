d59ed0b8e0aa9c506ff9f65eb4298ec36fe5eab6
CPL <carlos@centropyme.com>
2025-05-30 08:37:12 +0200

    * Utilizamos @Autowired

--- src/main/java/org/cpl_cursos/jdbcspring/JdbcSpringApplication.java
+++ src/main/java/org/cpl_cursos/jdbcspring/JdbcSpringApplication.java
@@ -1,13 +1,24 @@
 package org.cpl_cursos.jdbcspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class JdbcSpringApplication {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
-        SpringApplication.run(JdbcSpringApplication.class, args);
+        ConfigurableApplicationContext contexto = SpringApplication.run(JdbcSpringApplication.class, args);
+        //JdbcTemplate jdbcTemp =  contexto.getBean(JdbcTemplate.class);
+
+        //System.out.println("Implementación del Pool de conexiones -> " + ds.getClass().getSimpleName());
     }
 
 }
